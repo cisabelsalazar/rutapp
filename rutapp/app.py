168186fac26132aa0acd1cb7c66efcc442d29c77
cisabelsalazar <isabelsalazar1589@gmail.com>
2026-03-05 16:46:38 -0500

    Actualización
    Actualización conexión Flask html css

--- rutapp/app.py
+++ rutapp/app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+# Página de login
+@app.route('/')
 def login():
-    return "<h1>RUTAPP funcionando 🚀</h1>"
+    return render_template('login.html')
 
-if __name__ == "__main__":
+# Panel administrador
+@app.route('/admin')
+def administrador():
+    return render_template('admin.html')
+
+# Panel conductor
+@app.route('/conductor')
+def conductor():
+    return render_template('conductor.html')
+
+# Panel padres
+@app.route('/padres')
+def padres():
+    return render_template('padres.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
+    

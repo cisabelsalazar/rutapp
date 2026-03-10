028427d1b08945ac693d699ce28169b848dbb075
cisabelsalazar <isabelsalazar1589@gmail.com>
2026-03-10 17:13:41 -0500

    Estructura inicial del proyecto RUTAPP con Flask y templates
    Actualización de repositorio

---
+++ basedatos/script_rutapp_bdc.sql
@@ -0,0 +1,145 @@
+CREATE DATABASE rutapp_bd;
+USE rutapp_bd;
+
+DROP TABLE IF EXISTS ROL;
+
+CREATE TABLE ROL(
+	id_rol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre_rol VARCHAR(50) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS PERMISO;
+
+CREATE TABLE PERMISO(
+	id_permiso INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre_permiso VARCHAR(50) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS ROL_PERMISO;
+
+CREATE TABLE ROL_PERMISO(
+	id_rol INT NOT NULL,
+    id_permiso INT NOT NULL,
+    PRIMARY KEY(id_rol, id_permiso),
+    FOREIGN KEY (id_rol) REFERENCES ROL(id_rol) ON DELETE CASCADE,
+    FOREIGN KEY (id_permiso) REFERENCES PERMISO(id_permiso) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS USUARIO;
+
+CREATE TABLE USUARIO(
+	id_usuario VARCHAR(20) NOT NULL PRIMARY KEY,
+    nombre_usuario VARCHAR(50) NOT NULL UNIQUE,
+	hash_password VARCHAR(255) NOT NULL,
+    nombres_y_apellidos VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    telefono VARCHAR(20) NOT NULL,
+    id_rol INT NOT NULL,
+    FOREIGN KEY (id_rol) REFERENCES ROL(id_rol)
+);
+
+DROP TABLE IF EXISTS VEHICULO;
+
+CREATE TABLE VEHICULO(
+id_vehiculo INT AUTO_INCREMENT PRIMARY KEY,
+placa VARCHAR(10) NOT NULL UNIQUE,
+marca VARCHAR(50) NOT NULL,
+modelo VARCHAR(50) NOT NULL,
+capacidad INT NOT NULL,
+id_conductor VARCHAR(20) NOT NULL,
+FOREIGN KEY (id_conductor) REFERENCES USUARIO(id_usuario)
+);
+
+DROP TABLE IF EXISTS RUTA;
+
+CREATE TABLE RUTA(
+	id_ruta INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_ruta VARCHAR(50) NOT NULL UNIQUE,
+    descripcion_ruta VARCHAR(100) NOT NULL,
+    hora_salida TIME NOT NULL,
+    hora_llegada TIME NOT NULL,
+    id_conductor VARCHAR(20) NOT NULL,
+    id_administrador VARCHAR(20) NOT NULL,
+    FOREIGN KEY (id_conductor) REFERENCES USUARIO(id_usuario),
+    FOREIGN KEY (id_administrador) REFERENCES USUARIO(id_usuario)
+);
+
+DROP TABLE IF EXISTS ESTUDIANTE;
+
+CREATE TABLE ESTUDIANTE(
+	id_estudiante INT PRIMARY KEY NOT NULL,
+    nombre VARCHAR(80) NOT NULL,
+    grado VARCHAR(20) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20),
+    id_ruta INT NULL,
+    FOREIGN KEY (id_ruta) REFERENCES RUTA(id_ruta) ON DELETE SET NULL
+);
+
+--- QUERY 
+SELECT * FROM rol ORDER BY id_rol;
+SELECT * FROM permiso ORDER BY id_permiso;
+SELECT * FROM rol_permiso ORDER BY id_rol;
+SELECT * FROM usuario ORDER BY id_usuario;
+SELECT * FROM ruta ORDER BY id_ruta;
+SELECT * FROM estudiante ORDER BY id_estudiante;
+
+
+-- INFORMACION BASE DE DATOS - ROL
+INSERT INTO `rutapp_bd`.`rol` (`id_rol`, `nombre_rol`) VALUES ('1', 'Super Administrador');
+INSERT INTO `rutapp_bd`.`rol` (`id_rol`, `nombre_rol`) VALUES ('2', 'Administrador');
+INSERT INTO `rutapp_bd`.`rol` (`id_rol`, `nombre_rol`) VALUES ('3', 'Conductor');
+INSERT INTO `rutapp_bd`.`rol` (`id_rol`, `nombre_rol`) VALUES ('4', 'Padre de Familia');
+
+
+-- INFORMACION BASE DE DATOS - PERMISOS DEL ROL
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('1', 'Registro de Usuario');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('2', 'Inicio de Sesión');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('3', 'Gestionar Usuarios');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('4', 'Gestionar Estudiantes');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('5', 'Gestionar Vehículos');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('6', 'Gestionar Rutas');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('7', 'Compartir Ubicación');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('8', 'Visualizar Rutas');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('9', 'Monitorear Ruta en Tiempo Real');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('10', 'Gestión de Alertas');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('11', 'Visualizar Información de Ruta');
+INSERT INTO `rutapp_bd`.`permiso` (`id_permiso`, `nombre_permiso`) VALUES ('12', 'Reportar Inasistencia');
+
+
+-- INFORMACION BASE DE DATOS - ROL_PERMISO
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '1');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '2');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '3');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '4');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '5');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '6');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '7');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '8');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '9');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '10');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '11');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('1', '12');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '1');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '2');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '3');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '4');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '5');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '6');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '8');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '9');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '10');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('2', '11');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('3', '2');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('3', '7');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('3', '9');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('3', '10');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('4', '2');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('4', '9');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('4', '10');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('4', '11');
+INSERT INTO `rutapp_bd`.`rol_permiso` (`id_rol`, `id_permiso`) VALUES ('4', '12');
+
+
+-- select md5('12345') from dual;
+INSERT INTO `rutapp_bd`.`usuario` (`id_usuario`, `nombre_usuario`, `hash_password`, `nombres_y_apellidos`, `correo`, `telefono`, `id_rol`) VALUES ('1', 'SuperAdministrador', md5('12345'), 'Cristina Salazar', 'isabelsalazar1589@gmail.com', '3164539219', '1');
--- rutapp/app.py
+++ rutapp/app.py
@@ -1,27 +1,126 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for,session
+import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "rutapp_secreto"
+
+#Esta es la conexión a la base de datos MySQL
+
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Cristina+-2026",
+    database="rutapp_bd"
+)
 
 # Página de login
 @app.route('/')
 def login():
     return render_template('login.html')
 
+#Usuario
+@app.route('/usuarios')
+def usuarios():
+
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    
+    if session['rol'] !=1:
+        return "Acceso no autorizado"
+    
+    cursor = conexion.cursor(dictionary=True)
+
+    consulta = """ 
+    SELECT id_usuario,
+       nombre_usuario,
+       nombres_y_apellidos,
+       correo,
+       telefono,
+       id_rol
+    FROM usuario
+    """
+
+    cursor.execute(consulta)
+    lista_usuarios = cursor.fetchall()
+
+    return render_template('usuarios.html', usuarios = lista_usuarios)
+ 
+
 # Panel administrador
 @app.route('/admin')
 def administrador():
+
+    if 'usuario' not in session:
+        return redirect(url_for('login')) #Si no hay sesión iniciada regresa al login
+    
+    if session['rol'] != 1:
+        return "Acceso no autorizado"
+    
     return render_template('admin.html')
 
 # Panel conductor
 @app.route('/conductor')
 def conductor():
+
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+
+    if session['rol'] != 2:
+        return "Acceso no autorizado"
+        
     return render_template('conductor.html')
 
 # Panel padres
 @app.route('/padres')
 def padres():
+
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    
+    if session['rol'] != 3:
+        return "Acceso no autorizado"
+    
     return render_template('padres.html')
+
+@app.route('/validar_login',methods=['POST'])
+def valida_login():
+
+    correo = request.form['correo']
+    password = request.form['password']
+
+    cursor = conexion.cursor(dictionary=True)
+
+    consulta = """
+    SELECT *
+    FROM usuario
+    WHERE correo=%s AND hash_password = md5(%s)
+    """
+
+    cursor.execute(consulta,(correo,password))
+    usuario = cursor.fetchone()
+
+    if usuario:
+        id_rol = usuario[6] # Esta es la posición en donde esta el usuario en la tabla BD
+
+        session['usuario'] = usuario[0]
+        session['rol'] = id_rol
+
+        if id_rol == 1:
+            return redirect(url_for('administrador'))
+        elif id_rol == 2:
+            return redirect(url_for('conductor'))
+        elif id_rol == 3:
+            return redirect(url_for('padres'))
+        else:
+            return "Rol no reconocido"
+
+    else:
+        return "Usuario o contraseña incorrectos"
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
